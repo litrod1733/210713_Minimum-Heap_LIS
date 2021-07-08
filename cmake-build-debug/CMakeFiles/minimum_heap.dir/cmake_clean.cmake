7ae7fc19115645e302407bbd6ce0e230b9b77508
이인성 <isaac@iinseongs-MacBook-Pro.local>
2021-07-08 22:06:11 +0900

    solution

---
+++ cmake-build-debug/CMakeFiles/minimum_heap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minimum_heap.dir/main.cpp.o"
+  "minimum_heap"
+  "minimum_heap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/minimum_heap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -2,3 +2,28 @@
 // Created by 이인성 on 2021/07/08.
 //
 
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+  priority_queue<int, vector<int>, greater<>> pq;
+  int N;
+  int x;
+  cin >> N;
+  for(int i=0; i<N; i++) {
+    cin >> x;
+    if(x == 0) {
+      if(pq.empty())
+        cout << "0" << endl;
+      else {
+        cout << pq.top() << endl;
+        pq.pop();
+      }
+    }
+    else
+      pq.push(x);
+  }
+  return 0;
+}

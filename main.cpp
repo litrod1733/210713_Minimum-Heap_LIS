9f0d70a46171864c780c4899481d6022c42c3e17
이인성 <isaac@iinseongs-MacBook-Pro.local>
2021-07-10 00:01:42 +0900

    solution

--- main.cpp
+++ main.cpp
@@ -3,27 +3,36 @@
 //
 
 #include <iostream>
+#include <cstdio>
 #include <queue>
 
 using namespace std;
 
 int main() {
+//  ios_base :: sync_with_stdio(false);
+//  cin.tie(NULL);
+//  cout.tie(NULL);
   priority_queue<int, vector<int>, greater<>> pq;
   int N;
   int x;
-  cin >> N;
+//  cin >> N;
+  scanf("%d", &N);
   for(int i=0; i<N; i++) {
-    cin >> x;
+//    cin >> x;
+    scanf("%d", &x);
     if(x == 0) {
       if(pq.empty())
-        cout << "0" << endl;
+//        cout << 0 << endl;
+        printf("%d\n", 0);
       else {
-        cout << pq.top() << endl;
+//        cout << pq.top() << endl;
+        printf("%d\n", pq.top());
         pq.pop();
       }
     }
     else
       pq.push(x);
   }
+
   return 0;
 }
